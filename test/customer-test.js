7acb2f7b007d51bf4a7467c25f8e268793bfb188
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-11-13 13:06:22 -0700

    tests all properties of Customer class

---
+++ test/customer-test.js
@@ -0,0 +1,40 @@
+import chai from 'chai'
+import Customer from '../src/Customer'
+import { customerSample } from './customer-sample'
+
+const expect = chai.expect
+
+let customer1, customer2
+
+describe('Customer Class', function() {
+    beforeEach(() => {
+        customer1 = new Customer(customerSample[0])
+        customer2 = new Customer(customerSample[1])
+    })
+
+  it('should set customer id', function() {
+    expect(customer1.id).to.equal(1)
+    expect(customer2.id).to.equal(2)
+  })
+
+  it('should set customer name', function() {
+    expect(customer1.name).to.equal('Leatha Ullrich')
+    expect(customer2.name).to.equal('Rocio Schuster')
+  })
+
+  it('should set pastBookings property to an empty array', function() {
+    expect(customer1.pastBookings).to.deep.equal([])
+    expect(customer2.pastBookings).to.deep.equal([])
+  })
+
+  it('should set upcomingBookings property to an empty array', function() {
+    expect(customer1.upcomingBookings).to.deep.equal([])
+    expect(customer2.upcomingBookings).to.deep.equal([])
+  })
+
+  it('should set allBookings property to an empty array', function() {
+    expect(customer1.allBookings).to.deep.equal([])
+    expect(customer2.allBookings).to.deep.equal([])
+  })
+
+})
