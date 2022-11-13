f2971c84a8d84f95a45f8a59c426fb29b918c09c
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-11-13 12:49:44 -0700

    created customer sample file

---
+++ test/customer-sample.js
@@ -0,0 +1,14 @@
+export const customerSample = [
+    {
+        id: 1,
+        name: "Leatha Ullrich"
+    },
+    {
+        id: 2,
+        name: "Rocio Schuster"
+    },
+    {
+        id: 3,
+        name: "Kelvin Schiller"
+    }
+]
